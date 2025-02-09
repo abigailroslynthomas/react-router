5f2b15911571fbc3ddeab5112c69a7a5c565a1b5
Abigail Thomas <abigailthomas@Abigails-MacBook-Air.local>
2025-02-09 17:33:36 -0500

    inital commit

---
+++ src/BluePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BluePage() {
+  return (
+    <div className="blue-background">
+      <h1>Blue Page</h1>
+    </div>
+  );
+}
+
+export default BluePage;
---
+++ src/RedPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RedPage() {
+  return (
+    <div className="red-background">
+      <h1>Red Page</h1>
+    </div>
+  );
+}
+
+export default RedPage;
