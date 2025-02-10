b1f9dce792b7797a6fd64edc0c84790cfa3ae8ee
Abigail Thomas <abigailthomas@Abigails-MacBook-Air.local>
2025-02-10 09:27:40 -0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './index.css';
+import HomePage from './pages/HomePage';
+import BluePage from './pages/BluePage';
+import RedPage from './pages/RedPage';
 
 function App() {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+    <Router>
+      <nav>
+        <Link to="/" style={{ margin: '0 10px' }}>Home</Link>
+        <Link to="/blue" style={{ margin: '0 10px' }}>Blue</Link>
+        <Link to="/red" style={{ margin: '0 10px' }}>Red</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/blue" element={<BluePage />} />
+        <Route path="/red" element={<RedPage />} />
+      </Routes>
+    </Router>
   );
 }
 
